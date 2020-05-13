84c51044ef1edf5efe5357f1354f4ed986fa1ec6
Vela Farquharson <vela@bleachr.co>
2020-05-12 21:13:43 -0500

    Use proper logo and stub in some nav items

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.scss';
-import logo from './Dragana-Jevtovic-Logo.png';
+import logo from './logo.png';
 const Header = () => {
     return (
         <div className="header-container">
             <img src={logo} alt=""/>
+            <ul className="nav-items">
+                <li>Home</li>
+                <li>Products</li>
+                <li>Contact</li>
+            </ul>
         </div>
     )
 }
