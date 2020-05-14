00bc13d07bef22808b472838f1c99efbfdf07c12
Vela Farquharson <vela@bleachr.co>
2020-05-13 19:41:21 -0500

    Comment out auth from header for now

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Header.scss';
 import logo from './logo.png';
 import { connect } from 'react-redux';
-import SignedInLinks from '../SignedInLinks';
-import SignedOutLinks from '../SignedOutLinks';
+// import SignedInLinks from '../SignedInLinks';
+// import SignedOutLinks from '../SignedOutLinks';
 const Header = (props) => {
-    const { auth } = props;
+    // const { auth } = props;
     return (
         <div className="header-container">
-            { auth.uid ? <SignedInLinks/> : <SignedOutLinks /> }
+            {/* { auth.uid ? <SignedInLinks/> : <SignedOutLinks /> } */}
             <img src={logo} alt=""/>
             <ul className="nav-items">
                 <li>Home</li>
