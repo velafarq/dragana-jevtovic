85f263263020c879550a3505f0666e0fd05066b6
Vela Farquharson <vela@bleachr.co>
2020-05-17 14:00:35 -0500

    Search filter working

--- src/components/admin/admin-products/AdminProducts.jsx
+++ src/components/admin/admin-products/AdminProducts.jsx
@@ -13,6 +13,7 @@ const AdminProducts = () => {
     const [displayProducts, setDisplayProducts] = useState([]);
     const [typeFilter, setTypeFilter] = useState('');
     const [designFilter, setDesignFilter] = useState('');
+    const [searchWord, setSearchWord] = useState('');
 
     useEffect(() => {
         setDisplayProducts(allProducts);
@@ -32,25 +33,65 @@ const AdminProducts = () => {
     
     const filter = (category, value) => {
         let filtered = allProducts;
-        if (category === 'design'){
+        if (category === 'design') {
             setDesignFilter(value);
             if (value) {
-                filtered = filtered.filter(p => p.design === value);
+                filtered = filterDropdown(filtered, value, 'design');
             }
             if (typeFilter) {
-                filtered = filtered.filter(p => p.type === typeFilter);
+                filtered = filterDropdown(filtered, typeFilter, 'type');
+            }
+            if (searchWord) {
+                filtered = filterString(filtered, searchWord);
             }
         } else if (category === 'type') {
             setTypeFilter(value);
             if (value) {
-                filtered = filtered.filter(p => p.type === value);
+                filtered = filterDropdown(filtered, value, 'type');
             }
             if (designFilter) {
-                filtered = filtered.filter(p => p.design === designFilter);
+                filtered = filterDropdown(filtered, designFilter, 'design');
+            }
+            if (searchWord) {
+                filtered = filterString(filtered, searchWord);
+            }
+        } else if (category === 'search') {
+            setSearchWord(value);
+
+            if (value) {
+                filtered = filterString(filtered, value);
+            }
+            if (typeFilter) {
+                filtered = filterDropdown(filtered, typeFilter, 'type');
+            }
+            if (designFilter) {
+                filtered = filterDropdown(filtered, designFilter, 'design');
             }
         }
         setDisplayProducts(filtered);
     }
+
+    const filterDropdown = (data, filter, key) => {
+        return data.filter(p => p[key] === filter);
+    }
+
+    const filterDesign = (data, filter) => {
+        return data.filter(p => p.design === filter);
+    }
+
+    const filterString = (data, filter) => {
+        return data.filter(p => {
+            return Object.values(p).some(val => {
+                if (typeof val === 'string') {
+                    const tidyVal = val.toLowerCase().trim();
+                    return tidyVal.includes(filter.toLowerCase().trim());
+                } 
+                return false;
+            });
+        });
+    }
+
+
 
     const resetFilter = () => {
         setDisplayProducts(allProducts);
@@ -64,6 +105,10 @@ const AdminProducts = () => {
             <Fragment>
                 <button onClick={() => resetFilter()}>Reset</button>
                 {/* filters */}
+                <div className="form-input">
+                    <label htmlFor="search">Search</label>
+                    <input type="text" value={searchWord} onChange={(event) => filter('search', event.target.value)} id='search' />
+                </div>
                 <div className="form-input">
                     <label htmlFor="type">Filter by Type</label>
                     <select id="type" value={typeFilter} onChange={(event) => filter('type', event.target.value)} >
