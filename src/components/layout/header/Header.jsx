07c00b031e1dcac6e631b0ceeb356cdd3ef54916
Vela Farquharson <vela.mrdjen@gmail.com>
2020-05-25 12:13:02 -0500

    Header layout

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.scss';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 // import SignedInLinks from '../SignedInLinks';
 // import SignedOutLinks from '../SignedOutLinks';
 const Header = (props) => {
@@ -8,12 +9,17 @@ const Header = (props) => {
     return (
         <div className="header-container">
             {/* { auth.uid ? <SignedInLinks/> : <SignedOutLinks /> } */}
-            <img src='https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/logo%2Flogo.png?alt=media&token=24d2acb4-4fd9-4590-a5fe-164dd2169f9c' alt=""/>
-            <ul className="nav-items">
-                <li>Home</li>
-                <li>Products</li>
-                <li>Contact</li>
-            </ul>
+            <img className='logo' src='https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/logo%2Flogo.png?alt=media&token=24d2acb4-4fd9-4590-a5fe-164dd2169f9c' alt=""/>
+            <nav className="nav-items">
+                <NavLink className='item' exact to='/' activeClassName='selected'>HOME</NavLink>
+                <NavLink className='item' strict to='/products' activeClassName='selected'>PRODUCTS</NavLink>
+                <NavLink className='item' exact to='/about' activeClassName='selected'>ABOUT</NavLink>
+                <NavLink className='item' exact to='/contact' activeClassName='selected'>CONTACT</NavLink>
+            </nav>
+            <div className="side-icons">
+                <i className="material-icons">search</i>
+                <NavLink exact to='/checkout' className="item"><i className="material-icons">shopping_cart</i></NavLink>
+            </div>
         </div>
     )
 }
