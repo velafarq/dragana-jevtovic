06d7e98dd2033094dd0224bd8149245ba01bd3c0
Vela Farquharson <vela.mrdjen@gmail.com>
2020-06-11 15:54:35 -0500

    Add hidden to filters

--- src/components/admin/product-filter/ProductFilter.jsx
+++ src/components/admin/product-filter/ProductFilter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './ProductFilter.scss';
-import { typeOptions, designOptions, DisplayOptions} from '../../../helpers';
+import { typeOptions, designOptions, DisplayOptions, hiddenOptions, setStringToBool} from '../../../helpers';
 
 const ProductFilter = (props) => {
     const { allProducts, setDisplayProducts } = props;
@@ -8,49 +8,71 @@ const ProductFilter = (props) => {
     const [typeFilter, setTypeFilter] = useState('');
     const [designFilter, setDesignFilter] = useState('');
     const [searchWord, setSearchWord] = useState('');
+    const [hiddenFilter, setHiddenFilter] = useState('all');
+
+    const handleFilters = (filtered, omit) => {
+        if (typeFilter && omit !== 'type') {
+            filtered = filterDropdown(filtered, typeFilter, 'type');
+        }
+        if (searchWord && omit !== 'search') {
+            filtered = filterString(filtered, searchWord);
+        }
+        if (hiddenFilter !== 'all' && omit !== 'hidden') {
+            filtered = filterDropdown(filtered, hiddenFilter, 'hidden');
+        }
+        if (designFilter && omit !== 'design') {
+            filtered = filterDropdown(filtered, designFilter, 'design');
+        }
+        
+        return filtered;
+    }
 
     const filter = (category, value) => {
         let filtered = allProducts;
+        switch (category) {
+            case 'design':
+                setDesignFilter(value);
+                if (value) {
+                    filtered = filterDropdown(filtered, value, 'design');
+                }
+                filtered = handleFilters(filtered, 'design');
+                break;
+            case 'type':
+                setTypeFilter(value);
+                if (value) {
+                    filtered = filterDropdown(filtered, value, 'type');
+                }
+                filtered = handleFilters(filtered, 'type');
+                break;
+            case 'search':
+                setSearchWord(value);
+                if (value) {
+                    filtered = filterString(filtered, value);
+                }
+                filtered = handleFilters(filtered, 'search');
+                break;
+            case 'hidden':
+                const boolVal = setStringToBool(value);
+                setHiddenFilter(boolVal);
 
-        if (category === 'design') {
-            setDesignFilter(value);
-            if (value) {
-                filtered = filterDropdown(filtered, value, 'design');
-            }
-            if (typeFilter) {
-                filtered = filterDropdown(filtered, typeFilter, 'type');
-            }
-            if (searchWord) {
-                filtered = filterString(filtered, searchWord);
-            }
-        } else if (category === 'type') {
-            setTypeFilter(value);
-            if (value) {
-                filtered = filterDropdown(filtered, value, 'type');
-            }
-            if (designFilter) {
-                filtered = filterDropdown(filtered, designFilter, 'design');
-            }
-            if (searchWord) {
-                filtered = filterString(filtered, searchWord);
-            }
-        } else if (category === 'search') {
-            setSearchWord(value);
-            if (value) {
-                filtered = filterString(filtered, value);
-            }
-            if (typeFilter) {
-                filtered = filterDropdown(filtered, typeFilter, 'type');
-            }
-            if (designFilter) {
-                filtered = filterDropdown(filtered, designFilter, 'design');
-            }
+                if (value !== 'all') {
+                    filtered = filterDropdown(filtered, boolVal, 'hidden');
+                }
+                filtered = handleFilters(filtered, 'hidden');
+                break;
+            default: 
+                break;
         }
+
         setDisplayProducts(filtered);
     }
 
     const filterDropdown = (data, filter, key) => {
-        return data.filter(p => p[key] === filter);
+        if (key === 'hidden') {
+            return data.filter(p => p[key] === filter);
+        } else {
+            return data.filter(p => p[key] === filter);
+        }
     }
 
     const filterString = (data, filter) => {
@@ -70,6 +92,7 @@ const ProductFilter = (props) => {
         setDesignFilter('');
         setTypeFilter('');
         setSearchWord('');
+        setHiddenFilter('all');
     }
 
     return (
@@ -90,6 +113,12 @@ const ProductFilter = (props) => {
                     <label htmlFor="design">Filter by Design</label>
                     <select id="design" value={designFilter} onChange={(event) => filter('design', event.target.value)} >
                         <DisplayOptions options={designOptions} />
+                    </select>
+                </div>
+                <div className="form-input">
+                    <label htmlFor="hidden">Filter by Hidden</label>
+                    <select id="hidden" value={hiddenFilter} onChange={(event) => filter('hidden', event.target.value)} >
+                        <DisplayOptions options={hiddenOptions} />
                     </select>
                 </div>
             </div>
--- src/helpers.js
+++ src/helpers.js
@@ -5,7 +5,7 @@ export const isAdminWhiteList = (uid) => {
 }
 
 export const designOptions = [
-    { value:'', label: ''},
+    { value:'', label: 'All'},
     { value:'blue_guinea', label: 'Blue Guinea Fowl'},
     { value:'royal_african', label: 'Royal African'},
     { value:'brown_feather', label: 'Brown Feather'},
@@ -14,7 +14,7 @@ export const designOptions = [
 ];
 
 export const typeOptions = [
-    { value:'', label: ''},
+    { value:'', label: 'All'},
     { value:'plates', label: 'Plates'},
     { value:'bowls', label: 'Bowls'},
     { value:'teapots', label: 'Teapots'},
@@ -24,8 +24,24 @@ export const typeOptions = [
     { value:'condiments', label: 'Condiments'}
 ];
 
+export const hiddenOptions = [
+    { value: 'all', label: 'All'},
+    { value: false, label: 'Displayed'},
+    { value: true, label: 'Hidden'}
+];
+
 export const DisplayOptions = ({ options }) => {
     return options.map((option, idx) => (
         <option key={idx} value={option.value}>{option.label}</option>
     ))
 };
+
+export const setStringToBool = (string) => {
+    if (string === 'true') {
+        return true;
+    }
+    if (string === 'false') {
+        return false;
+    }
+    return 'all';
+}
