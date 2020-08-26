cc7a21f120a025a903b8be837b407f3380bd7476
Vela Farquharson <vela.mrdjen@gmail.com>
2020-08-26 17:24:09 -0500

    Initial work for checkout page

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,15 +1,78 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import './Checkout.scss'
 import { connect } from 'react-redux';
 import { submitOrder } from '../../store/actions/cart-actions';
+import { DESIGN_NAMES, handlePrice } from '../../helpers';
 
 const Checkout = (props) => {
-
+    console.log(props.items)
     const handleCheckout = () => {
         props.submitOrder(props.items);
     }
+
+    const getPrimaryImage = (item) => {
+        if (item.images && item.images.length) {
+            const image = item.images.find(img => img.primary);
+            return image.url;
+        }
+        return '';
+    }
+
+    const calculateTotal = (price, qty) => {
+        const total = price * qty;
+        return handlePrice(total, 'usd'); 
+    }
+
+    const removeItemFromCart = (item) => {
+        console.log(item);
+    }
+
+    function handleInput(e, itemDetails) {
+        itemDetails.quantity = e.target.value;
+    }
+
+    const row = (itemDetails) => {
+        const { item, quantity } = itemDetails;
+        return (
+            <Fragment key={item.id}>
+                <div className='box'><img src={getPrimaryImage(item)} alt="" /></div>
+                <div className='box'>
+                    <div>{DESIGN_NAMES[item.design]}</div>
+                    <div>{item.name}</div>
+                </div>
+                <div className='box'>{handlePrice(item.price, 'usd')}</div>
+                <div className="box">
+                    <input type="number" value={quantity} onChange={(e) => handleInput(e, itemDetails)} min="1" />
+                </div>
+                <div className="box">{calculateTotal(item.price['usd'], quantity)}</div>
+                <div className="box table-actions">
+                    <i className="material-icons close" onClick={() => removeItemFromCart(item)}>close</i>
+                </div>
+            </Fragment>);
+
+    }
+
+
+    const generateTable = (products) => {
+        return products.map((product) => row(product));
+    }
+
     return (
-        <div>
+        <div className="checkout">
+            <h1 className="heading-text checkout__title">Order Request</h1>
             <button onClick={() => handleCheckout()}>Checkout</button>
+
+            <div className="table">
+                <div className="box title"></div>
+                <div className="box title"></div>
+                <div className="box title">Price</div>
+                <div className="box title">Qty</div>
+                <div className="box title">Total</div>
+                <div className="box title"></div>
+
+                {generateTable(props.items)}
+
+            </div>
         </div>
     )
 }
