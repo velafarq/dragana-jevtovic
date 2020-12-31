e23337037e73beeedcf3a4647451298e4d82195d
Vela Farquharson <vela.mrdjen@gmail.com>
2020-12-31 12:08:30 -0600

    Update home slider;

--- src/components/home/hero-slider/HeroSlider.jsx
+++ src/components/home/hero-slider/HeroSlider.jsx
@@ -7,27 +7,28 @@ class HeroSlider extends Component {
     state =  {
         items: [
         {
-            id: 0,
             title: 'Blue Guinea Fowl',
             design: 'blue_guinea',
             url: 'https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/home%2Fmain-slider%2Fblue-guinea-slide.png?alt=media&token=c4d35c3a-ef30-4051-aa05-35b4e08f2a19'
         },
         {
-            id: 1,
+            title: "Royal African",
+            design: 'royal_african',
+            url: 'https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/home%2Fmain-slider%2Froyal-african-main.png?alt=media&token=b6c6a8f9-d85f-43f8-838d-e4e3657fff78',
+        },
+        {
+            title: "African Elephant",
+            design: 'african_elephant',
+            url: 'https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/home%2Fmain-slider%2Fafrican-elephant-main.png?alt=media&token=05f67e2d-c364-4ee7-9341-e7a4f148ec60',
+        },
+        {
             title: 'Brown Feather',
             design: 'brown_feather',
             url: 'https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/home%2Fmain-slider%2Fbrown-feather-slide.JPG?alt=media&token=bda511ac-38a0-46e1-9533-d859edce351d'},
         {
-            id: 2,
             title: "Two Oceans' Feathers",
             design: 'oceans_feather',
             url: 'https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/home%2Fmain-slider%2Foceans-feathers-slider.JPG?alt=media&token=110083ff-3d26-49bc-86c8-25527f081e32'
-        },
-        {
-            id: 3,
-            title: "Royal African",
-            design: 'royal_african',
-            url: 'https://firebasestorage.googleapis.com/v0/b/dragana-jevtovic.appspot.com/o/home%2Fmain-slider%2Froyal-african-hero.jpg?alt=media&token=c287cb26-1d29-4632-b468-638439d96a58',
         }]
     }
 
@@ -43,10 +44,10 @@ class HeroSlider extends Component {
                     enableAutoPlay={false}
                     autoPlaySpeed={5000}
                     transitionMs={1500}>
-                    {items.map(item => 
+                    {items.map((item, i) => 
                     <div className='slide' 
                         style={{backgroundImage: `url('${item.url}')`}} 
-                        key={item.id}>
+                        key={i}>
                         <i className='material-icons arrow left' onClick={() => this.carousel.slidePrev()}>keyboard_arrow_left</i>
                         <i className='material-icons arrow right' onClick={() => this.carousel.slideNext()}>keyboard_arrow_right</i>
                         <div className="content">
