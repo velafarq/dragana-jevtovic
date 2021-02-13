9cad4e9b39dcaa99d894a300953a5c9054283d56
Vela Farquharson <vela.mrdjen@gmail.com>
2021-02-13 17:00:40 -0600

    Fix name for product box

--- src/components/products/ProductListing.jsx
+++ src/components/products/ProductListing.jsx
@@ -18,6 +18,15 @@ const ProductListing = ({ product, currency }) => {
     const image = setImage();
 
 
+    const handleCategoryLabels = (categories) => {
+        const labels = [];
+        categories.forEach(cat => {
+            labels.push(DESIGN_NAMES[cat]);
+        });
+
+        return labels.join(', ');
+    }
+
     return (
         <article className="product-listing">
             <div className="product-listing__img">
@@ -26,7 +35,7 @@ const ProductListing = ({ product, currency }) => {
                     'Product image coming soon!'
                 }
             </div>
-            <div className="product-listing__design box-padding">{DESIGN_NAMES[product.design]}</div>
+            <div className="product-listing__design box-padding">{handleCategoryLabels(product.categories)}</div>
             <div className="product-listing__title box-padding">{product.name}</div>
             <div className="product-listing__price box-padding">{handlePrice(product.price, currency)}</div>
         </article>
