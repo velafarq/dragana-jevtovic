44d9d40e506f9793f24ef38c1946161eea372677
Vela Farquharson <vela.mrdjen@gmail.com>
2021-03-01 07:54:00 -0600

    Add zoom to product image

--- src/components/product-gallery/ProductGallery.jsx
+++ src/components/product-gallery/ProductGallery.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProductGallery.scss';
 
 const ProductGallery = ({ image, toggleExpanded }) => {
+    const [zoom, setZoom] = useState(0);
+    const zoomIn = () => {
+        if (zoom < 10) {
+            setZoom(zoom + 1);
+        }
+    }
+
+    const zoomOut = () => {
+        if (zoom > 0) {
+            setZoom(zoom - 1);
+        }
+    }
     return (
         <div className="product-gallery">
+            <div className="zoom-btns">
+                <button onClick={() => zoomOut()}>
+                    <i className="material-icons">remove_circle_outline</i>
+                </button>
+                <button onClick={() => zoomIn()}>
+                    <i className="material-icons">add_circle_outline</i>
+                </button>
+            </div>
             <button className="close-btn" onClick={() => toggleExpanded()}>
                 <i className="material-icons">close</i>
             </button>
-            <img src={image.url} alt=""/>
+            <img id="main-gallery-image" className={'zoom-' + zoom} src={image.url} alt=""/>
         </div>
     )
 }
