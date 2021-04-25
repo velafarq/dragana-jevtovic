15263d0700efba97f0726cb2e47bac2c9b1ff5bd
Vela Farquharson <vela.mrdjen@gmail.com>
2021-04-25 17:38:08 -0500

    Create spinner component

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.scss';
+
+const Spinner = () => {
+    return (
+        <div class="lds-spinner">
+            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div>
+    );
+}
+
+export default Spinner;
